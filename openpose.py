0e54b4dffee539cf2696bdbe4f10e7ee3d36de49
Mauro Smaniotto <maloisio_otto@hotmail.com>
2022-09-22 00:16:55 -0300

    motion trail - DESKTOP

--- openpose.py
+++ openpose.py
@@ -1,10 +1,19 @@
 # To use Inference Engine backend, specify location of plugins:
 # export LD_LIBRARY_PATH=/opt/intel/deeplearning_deploymenttoolkit/deployment_tools/external/mklml_lnx/lib:$LD_LIBRARY_PATH
 import math
+from collections import deque
 import cv2 as cv
 import numpy as np
 import argparse
 from scipy.spatial import distance as dist
+import threading
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+                help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=32,
+                help="max buffer size")
+args1 = vars(ap.parse_args())
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', help='Path to image or video. Skip to capture frames from camera')
@@ -25,12 +34,17 @@ POSE_PAIRS = [["LShoulder", "RShoulder"], ["RShoulder", "RElbow"],
               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["LHip", "RHip"]]
 
 inWidth = 255
-inHeight = 255
+inHeight = 300
 net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
 
 cap = cv.VideoCapture("mauro4.mp4")
 pointsToAngle = []
 pointsList = []
+
+paintedPoints = deque(maxlen=300)
+savedPaintedPoints = []
+frameArray = []
+interestPoint = 0
 
 
 # cap = cv.imread("operator1.jpg")
@@ -42,7 +56,6 @@ pointsList = []
 
 # while cv.waitKey(1) < 0:
 # frame = cv.imread("mauro.mp4")
-
 
 def rescale_frame(frame, percent=50):
     width = int(frame.shape[1] * percent / 100)
@@ -98,70 +111,27 @@ def gradiant(pt1, pt2):
     return (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
 
 
-def mousePoints(event, x, y, flags, params):
-    if event == cv.EVENT_LBUTTONDOWN:
-        if len(pointsList) >= 3:
-            pointsList.clear()
-        cv.circle(frame, (x, y), 5, (0, 0, 255), cv.FILLED)
-        cv.imshow('Frame', frame)
-        pointsList.append([x, y])
-        print(pointsList)
-        getAngle(pointsList)
+def funcButton():
+    pass
 
-
-def drawBox(frame):
-    global boolean
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-    cv.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
-    print(bbox)
-    boolean = False
-    return x, y, w, h
-
-
-# hasFrame, frame = cap.read()
-# frame = rescale_frame(frame, percent=50)
-bbox = []
-
-
-# cv.destroyWindow("Tracking")
-
-def initConfig():
-    hasFrame, frame = cap.read()
-    frame = rescale_frame(frame, percent=50)
-    bbox = cv.selectROI("Frame", frame, False)
-    return bbox
-
-
-boolean = False
-
-
-def configScale(frame, boolean):
-    global bbox
-    if boolean == True:
-        bbox.append(cv.selectROI("Frame", frame, False))
-        x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-        return frame[y: y + h, x: x + w]
-
-
-paintedPoints = []
-savedPaintedPoints = []
 
 while True:
     hasFrame, frame = cap.read()
+    key = cv.waitKey(1)
 
-    # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #---------------loop video---------------
+    if not hasFrame:
+         cv.destroyAllWindows()
+         cap = cv.VideoCapture("mauro4.mp4")
+         hasFrame, frame = cap.read()
+
+    #frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame = rescale_frame(frame, percent=50)
-
-    # x, y, w, h = drawBox(frame)
-
-    # frame = configScale(frame, False)
 
     frame = frame[50:700, 300:730]
     frame = cv.resize(frame, [255, 255], interpolation=cv.INTER_BITS)
 
-    # frame3 = cv.normalize(frame, (255,255), 0 , cv.NORM_L1)
     frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-
     frame2 = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)  # criacao imagem preta
 
     frameWidth = frame.shape[1]
@@ -169,7 +139,7 @@ while True:
 
     net.setInput(cv.dnn.blobFromImage(frame, 0.5, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False))
     out = net.forward()
-    out = out[:7, :19, :-1, :-1]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+    out = out[:1, :19, :-1, :-1]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
 
     assert (len(BODY_PARTS) == out.shape[1])
 
@@ -197,72 +167,69 @@ while True:
         idTo = BODY_PARTS[partTo]
 
         if points[idFrom] and points[idTo]:
-            # if len(paintedPoints) <= 1:
-            paintedPoints.append(points[4])
-            savedPaintedPoints.append(points[4])
 
-            # else:
-            #   paintedPoints.clear()
-
-            # print(points[idTo][0])
             distEuc = dist.euclidean((points[idTo][0], points[idTo][1]), (points[idFrom][0], points[idFrom][1]))
             if distEuc <= 700:
-                # print(distEuc)
-                # print(points[idFrom])
-                # print(points[idTo])
                 cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 1)
                 cv.line(frame2, points[idFrom], points[idTo], (0, 255, 0), 1)
                 cv.imshow('Frame2', frame2)
-                # cv.putText(frameCrop, str(int(distEuc)), (int(points[idTo][0]), int(points[idTo][1])),
-                # cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
                 cv.ellipse(frame, points[idFrom], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
                 cv.ellipse(frame2, points[idFrom], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
                 cv.ellipse(frame, points[idTo], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
                 cv.ellipse(frame2, points[idTo], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
 
-                if paintedPoints[len(paintedPoints) - 1] is None:
-                    paintedPoints[len(paintedPoints) - 1] = paintedPoints[len(paintedPoints) - 2]
-                else:
-                    try:
-                        for paintedPoints[0] in paintedPoints:
-                            # cv.ellipse(frame, (paintedPoints[0][0], paintedPoints[0][1]), (2, 2), 0, 0, 360, (0, 255, 255), cv.FILLED)
-                            cv.ellipse(frame2, (paintedPoints[0][0], paintedPoints[0][1]), (2, 2), 0, 0, 360, (0, 255, 255),
-                                   cv.FILLED)
-                            if len(paintedPoints) > 2:
-                                for paintedPoints[0] in paintedPoints:
-                                 cv.line(frame, (paintedPoints[len(paintedPoints) - 2][0], paintedPoints[len(paintedPoints) - 2][1]),
-                                        (paintedPoints[len(paintedPoints) - 1][0],
-                                         paintedPoints[len(paintedPoints) - 1][1]), (0, 255, 255), 1)
-                    except:
-                        continue
+            # ----------------------DESENHA LINHA MOTION TRACKING--------------------
 
-                # if (points[4] and points[7]) is not None:
-                # if points[4][1] and points[7][1] < points[0][1]:
-                # print("esse " + str(points[4][1])+", "+ str(points[7][1]))
-                # print("esse " + str(points[0][1]))
-                # cv.putText(frame, "Assalto", (0,70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
-
-                # if (points[9] and points[12]) is not None:
-                #  print("Nao e none")
-                # if points[12][0] < points[9][0]:#quando peca cruzado
-                #    #cv.line(frame, points[idFrom], points[idTo], (0, 255, 255), 3)
-                #   cv.line(frame, points[9], points[8], (0, 255, 255), 3)
-                #  cv.line(frame, points[12], points[11], (0, 255, 255), 3)
-                # else:
-                #   print("nONE")
-
-            # cv.line(img2, points[idFrom], points[idTo], (0, 255, 0), 3)
-            # cv.ellipse(img2, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            # cv.ellipse(img2, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            if len(paintedPoints) == 20:
+            #mao direita
+            if key == ord("1"):
                 paintedPoints.clear()
+                interestPoint = 4
+            # mao equerda
+            if key == ord("2"):
+                paintedPoints.clear()
+                interestPoint = 7
+            # joelho direito
+            if key == ord("3"):
+                paintedPoints.clear()
+                interestPoint = 9
+            # joelho esquerdo
+            if key == ord("4"):
+                paintedPoints.clear()
+                interestPoint = 12
+            # quadril direito
+            if key == ord("5"):
+                paintedPoints.clear()
+                interestPoint = 8
+            # quadril esquerdo
+            if key == ord("6"):
+                paintedPoints.clear()
+                interestPoint = 11
+            #pe direito
+            if key == ord("7"):
+                paintedPoints.clear()
+                interestPoint = 10
+            #pe esquerdo
+            if key == ord("8"):
+                paintedPoints.clear()
+                interestPoint = 13
+
+            if interestPoint is not 0:
+                paintedPoints.appendleft(points[interestPoint])
+                savedPaintedPoints.append(points[interestPoint])
+
+            for i in np.arange(1, len(paintedPoints)):
+                if paintedPoints[i - 1] is None or paintedPoints[i] is None:
+                    continue
+                #thickness = int(np.sqrt(args1["buffer"] / float(i + 1)) * 2.5)
+                cv.line(frame, paintedPoints[i - 1], paintedPoints[i], (0, 0, 255), 2)
+                cv.line(frame2, paintedPoints[i - 1], paintedPoints[i], (0, 0, 255), 2)
+
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
     cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     cv.putText(frame, str(cv.CAP_PROP_FPS), (10, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
     # ------------------------ANGLES-------------------
-
     # cotovelo direito
     if (points[3] and points[2] and points[4]) is not None:
         if points[3][0] > points[4][0]:
@@ -294,37 +261,74 @@ while True:
     if len(pointsList) == 3:
         getAngle(pointsList)
 
-    frame = cv.resize(frame, [400, 400], interpolation=cv.INTER_BITS)
+    #frame = cv.resize(frame, [400, 400], interpolation=cv.INTER_BITS)
+    frameArray.append(frame2)
     cv.imshow('Frame', frame)
     cv.imshow('Frame2', frame2)
-
-    # cv.imshow('shadows_out.png', result)
-    # cv.imshow('shadows_out_norm.png', result_norm)
-
-    cv.setMouseCallback("Frame", mousePoints)
     heatMap = cv.resize(heatMap, [400, 400], interpolation=cv.INTER_BITS)
     cv.imshow('Pontos', heatMap)
 
-    key = cv.waitKey(1)
+
     if key == ord('q'):
         break
     if key == ord('p'):
         cv.waitKey(-1)  # wait until any key is pressed
+
+    #------------------MANIPULANDO VIDEO/FRAMES-----------------
     if key == ord('j'):
+        cv.destroyWindow('Frame')
+        cv.destroyWindow('Frame2')
+        cv.destroyWindow('Pontos')
         frame3 = np.zeros((255, 255, 3), np.uint8)
-        for savedPaintedPoints[0] in savedPaintedPoints:
-            cv.ellipse(frame3, (savedPaintedPoints[0][0], savedPaintedPoints[0][1]), (2, 2), 0, 0, 360,
-                           (0, 255, 255),
-                           cv.FILLED)
-            cv.imshow('Frame3', frame3)
+        cv.imshow('Frame3', frameArray[0])
+        counter = 0
+        i = 0
+        while key != ord('q'):
+
+            #-------------START/PAUSE------------------
+            if key == ord('p'):
+                while True:
+                    if i > 0:
+                        for i in np.arange(i, len(frameArray)):
+                            cv.imshow('Frame3', frameArray[i])
+                            key = cv.waitKey(50)
+                            if key == ord('r'):
+                                i = 0
+                                break
+                            if key == ord('p'):
+                                break
+                    else:
+                        for i in np.arange(1, len(frameArray)):
+                            cv.imshow('Frame3', frameArray[i])
+                            key = cv.waitKey(50)
+                            if key == ord('r'):
+                                i = 0
+                                break
+                            if key == ord('p'):
+                                break
+                    break
+
+            #-----------AVANÇAR FRAME-------------
+            if key == ord('l'):
+                if i < len(frameArray) -1:
+                    i = 1 + i
+                    cv.imshow('Frame3', frameArray[i])
+
+            #----------VOLTAR FRAME--------------
+            if key == ord('k'):
+                if i > 0:
+                    i = i - 1
+                    cv.imshow('Frame3', frameArray[i])
+
+            #----------RESTART------------------
+            if key == ord('r'):
+                i = 0
+                cv.imshow('Frame3', frameArray[i])
+
             key = cv.waitKey(1)
             if key == ord('q'):
                 break
-
-    if key == ord('r'):
-        configScale(frame, True)
-
-
+        cv.destroyAllWindows()
 
 cap.release()
 cv.destroyAllWindows()
